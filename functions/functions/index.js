63f5d37c06bd32f75f6fceb9b42a3afaaeff8727
Joshua Bienes <jbienes.dev@gmail.com>
2018-09-22 06:42:45 +0800

    Bot added

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,8 +1,16 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+const {WebhookClient} = require('dialogflow-fulfillment');
 const getTime = require('date-fns/get_time')
 const format = require('date-fns/format')
-admin.initializeApp()
+admin.initializeApp({
+  apiKey: "AIzaSyCUbLt0vI0Y6jwx7m1z0IPGdDI1DxhfULc",
+  authDomain: "arcpark-1532921739973.firebaseapp.com",
+  databaseURL: "https://arcpark-1532921739973.firebaseio.com",
+  projectId: "arcpark-1532921739973",
+  storageBucket: "arcpark-1532921739973.appspot.com",
+  messagingSenderId: "569429999510"
+})
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
@@ -67,6 +75,31 @@ exports.logTransaction = functions.database.ref('/parking_area/{areaNumber}/stat
       }
     })
   })
+
+exports.dialogflowAvailableSpaces = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers))
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body))
+
+  function checkAvailableSpace(agent) {
+    return admin.database().ref('parking_area').once('value', snap => {
+      let parkingAreas = []
+      snap.forEach(childSnap => {
+        parkingAreas.push(childSnap.val())
+      })
+      let availableParkingAreas = parkingAreas.filter(parkingArea => parkingArea.status === 'available')
+      if(availableParkingAreas.length > 0) {
+        agent.add(`There are ${availableParkingAreas.length} available parking space${availableParkingAreas.length > 1 ? 's' : ''}.`)
+      } else {
+        agent.add(`Sorry. The parking area is full.`)
+      }
+    })
+  }
+
+  let intentMap = new Map();
+  intentMap.set('Check available spaces', checkAvailableSpace);
+  agent.handleRequest(intentMap);
+})
 
 // Status node is changed to waiting
 // Sensor is now emitting ultrasonnic waves (every 15sec)
